5c9bf7a4e3537e694a665d1ef1c75edd4d011893
Jack <jack@noreply.com>
2024-02-02 10:29:41 +0700

    Initial Commit

---
+++ MemoryGame/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MemoryGame
+//
+//  Created by cn333 on 2/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by cn333 on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
